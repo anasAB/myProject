20c31fa5edee931d99903f5a41146dbcbb987302
anasAB <anas.222@hotmail.com>
2021-08-01 12:59:58 +0200

    - adding dataset interface - use dynamic values with myTemplate - check if one of the parameters is missing - throw alert if one of parameters is missing
    (i) src/App.tsx
    (+) src/Typing/Types.ts
    (+) src/chop.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import './App.css';
+import Chop from './chop';
+import { IDataset } from './Typing/Types';
+
+
+const dataset: IDataset = {
+  bread: "baguette",
+  cheese: "brie cheese",
+};
+
+const myTemplate: string = `I like to eat ${dataset.bread} and ${dataset.cheese}`;
+
+Chop(dataset, myTemplate);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       </header>
+        <p>Yara</p>
+      </header>
     </div>
   );
 }
